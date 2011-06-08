77ad69979a5ab12ad9e18847de4820542e03dfa2
John Maguire <john.maguire@gmail.com>
2011-06-08 19:36:45 +0200

    Add eclipse project and classpath

--- android/BabbleSink/src/com/purplehatstands/babblesink/PhoneStateReceiver.java
+++ android/BabbleSink/src/com/purplehatstands/babblesink/PhoneStateReceiver.java
@@ -10,7 +10,9 @@ public class PhoneStateReceiver extends BroadcastReceiver {
 
   @Override
   public void onReceive(Context context, Intent intent) {
+    Log.d(this.getClass().getCanonicalName(), "Phone state changed!");
     String newState = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+    Log.d(this.getClass().getCanonicalName(), "Phone state changed to:" + newState);
     if (newState == TelephonyManager.EXTRA_STATE_RINGING) {
       // Phone has started ringing.
       Log.d(this.getClass().getCanonicalName(), "Phone is ringing!");
